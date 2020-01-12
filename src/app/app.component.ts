4537985382d05d70a9ef8feaf14a97716f259cc5
dev.darshana991 <dev.darshana92@gmail.com>
2020-01-13 05:21:55 +0530

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'වීරයන්ගේ චාරිකාව';
+}
